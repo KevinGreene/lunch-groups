b87024495014ca9ed15d49c06f9d95c2dfe39ed4
Kevin Greene <kevin@spantree.net>
2014-08-04 18:53:02 -0500

    Added contact page, centered some text, updated some wording

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pairLunchingApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pairLunchingApp
+ */
+angular.module('pairLunchingApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
